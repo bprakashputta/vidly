7644e37217e3495e8a80af7bc25219f9812bb455
Putta Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-10-22 22:51:06 +0530

    Implemented CRUD Operations for Vidly

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Joi = require('joi');
+const {request, response} = require("express");
 const vidly = express();
 
 // Environment Conditions
@@ -31,6 +32,14 @@ const gernes = [
     }
 ];
 
+function validateSchema(gerne){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    return schema.validate(gerne);
+}
+
 // HOME Page of VIDLY
 vidly.get('/', (request,response)=>{
     return response.send("Welcome to VIDLY!!");
@@ -52,12 +61,66 @@ vidly.get('/api/gernes/:id', (request, response)=>{
 
 // POST METHOD to INSET new gerne into gernes
 vidly.post('/api/gernes/', (request, response)=>{
-    // This method will try to insert new gerne object 
+    // This method will try to insert new gerne object
     // into the list / database
-    const schema = Joi.object({
-        
-    })
-})
+
+    // Step 1
+    // Check if the gerne object passed in body is valid
+    const validateSchema = valdateSchema(request.body);
+    if(validateSchema.error){
+        return response.status(400).send(validateSchema.error.details[0].message);
+    }
+
+    // Step 2
+    // Create Gerne object
+    const gerne = {
+        id: gernes.length+1,
+        name: request.body.name
+    };
+    // Step 3
+    // Insert Gerne object into Gerne List
+    gernes.push(gerne);
+    return response.send(gernes);
+});
+
+
+// PUT METHOD to UPDATE gerne object in gernes
+vidly.put('/api/gernes/:id', (request, response)=>{
+    // Step 1
+    // Check if the object with given ID exists
+    const gerne = gernes.find(g => g.id === parseInt(request.params.id));
+    if(!gerne){
+        return response.status(400).send(`The gerne object with given ${request.params.id} does not exist`);
+    }
+    // Step 2
+    // Check if the schema object is valid
+    // Has all the required parameters
+    const validateRequestObject = validateSchema(request.body);
+    if(validateRequestObject.error){
+        return response.status(400).send(validateRequestObject.error.details[0].message);
+    }
+
+    // Step 3
+    // Update the gerne object
+    gerne.name = request.body.name;
+    return response.send(gernes);
+});
+
+// DELETE METHOD to DELETE gerne object from gernes
+vidly.delete('/api/gernes/:id', (request, response)=>{
+    // Step 1
+    // Check if the gerne object with given ID exists
+    const gerne = gernes.find(g => g.id === parseInt(request.params.id));
+    if(!gerne){
+        return response.status(400).send(`The object with given gerne ID ${request.params.id} does not exist`);
+    }
+
+    // Step 2
+    // Delete the gerne object
+    const index = gernes.indexOf(gerne);
+    gernes.splice(index, 1);
+    return response.send(gernes);
+});
 
 // Server is listening on PORT Number : port
 vidly.listen(port,()=>{
